515e8be086a42b934e48e4c81ceb15672436e984
Stefano Corra <stefano.corra@hotmail.com>
2018-04-25 15:57:42 -0400

    initial config

---
+++ src/components/posts_index.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+export default class PostsIndex extends Component {
+  render(){
+    return(
+      <div>
+        Posts Index
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
-import './index.css';
-import App from './App';
+import './style/index.css';
+import PostsIndex from './components/posts_index';
+
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleWare = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleWare(reducers)}>
-  <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root'));
 registerServiceWorker();

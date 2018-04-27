1cc4d286fecad4f7ceb9fd4a3e2525988381812f
Stefano Corra <stefano.corra@hotmail.com>
2018-04-27 00:09:23 -0400

    redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,52 @@
 import React,{Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
-  render(){
+
+  renderField(field){
     return(
-      <div>
-        PostsNew!
+      <div className="form-group">
+        <label htmlFor="">{field.label}</label>
+        <input className="form-control" type="text" {...field.input}/>
       </div>
     );
   }
+
+
+  render(){
+    return(
+      <form>
+        <Field label="Title for Post" name="title" component={this.renderField}/>
+        <Field label="Categories" name="categories" component={this.renderField}/>
+        <Field label="Post Content" name="content" component={this.renderField}/>
+      </form>
+    );
+  }
 }
-export default PostsNew;
+
+function validate(values){
+    const errors ={};
+
+    //validate input from 'values'
+    if(!values.title || values.title.length < 3){
+      errors.title ="Enter a title that is at least 3 characters!";
+    }
+
+    if (!values.categories){
+      errors.categories ="Enter some categories!";
+    }
+
+    if (!values.content){
+      errors.content ="Enter some content please!";
+    }
+
+
+    // if errors is empty, the form is fine to submit
+    // if errors has *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+  validate: validate,
+  form: 'PostsNewForm'
+})(PostsNew);
